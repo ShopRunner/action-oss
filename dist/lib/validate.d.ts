18e5d540e426aae8d0b3b12f289ef995c25f354e
Zac Rosenbauer <zrosenbauer@shoprunner.com>
2021-03-19 12:47:09 -0500

    add build

--- dist/lib/validate.d.ts
+++ dist/lib/validate.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="node" />
-export declare function validateLicense(fileContents: string): Promise<{
-    valid: boolean;
+export declare function validateLicense(fileContents?: Buffer): Promise<{
+    valid: boolean | "";
     errors: string[];
     currentYear?: string | undefined;
     license?: string | undefined;
 }>;
 export declare function validateLicenseFile(path: string): Promise<{
-    valid: boolean;
+    valid: boolean | "";
     errors: string[];
     currentYear?: string | undefined;
     license?: string | undefined;
